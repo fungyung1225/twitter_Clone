c22d488ae6436825ae0f809a8dfdb58ef27bb805
Fung Yung <fungyung1225@gmamil.com>
2019-03-07 09:06:32 -0500

    add login ban add tweet table view add tweet table controller

--- ex_twitter_Clone/Twitter/LoginViewController.swift
+++ ex_twitter_Clone/Twitter/LoginViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        loginBtn.layer.borderWidth = 1
+        loginBtn.layer.cornerRadius = 15
+        //loginBtn.layout
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onTapLogin(_ sender: Any) {
+        let urlString = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: urlString, success: {
+            //on login sucess, present homeTableVC
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Error. Could not login.")
+            let title = "Error"
+            let message = "An error has"
+            //let alert = UIAlertController
+        })
+        
     }
-    */
-
+    
 }
