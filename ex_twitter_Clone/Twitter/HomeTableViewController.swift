072b9be590e61a860a8f15ba883d230aed5e4e2d
Fung Yung <fungyung1225@gmamil.com>
2019-03-07 22:31:15 -0500

    add loadTweet function setup parameters and url for getting tweets get image from tweet json

--- ex_twitter_Clone/Twitter/HomeTableViewController.swift
+++ ex_twitter_Clone/Twitter/HomeTableViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class HomeTableViewController: UITableViewController {
 
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadTweet()//call function
+    }
+    
+    //create function for retrevie tweets
+    func loadTweet(){
+       let urlString = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
+        let myParams = ["count": 10] //for mutilple ["count": 10,"count": 10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: urlString, parameters: myParams, success: { (tweets:[NSDictionary]) in
+            //tweets is the return from api
+            self.tweetArray.removeAll()
+            for tweet in tweets{
+                self.tweetArray.append(tweet)//add tweet to dic
+            }
+            
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+            print("Count not retreive tweets!")
+        })
     }
 
     @IBAction func onTapLogout(_ sender: Any) {
@@ -25,19 +48,28 @@ class HomeTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
         
-        cell.userNameLbl.text = "Name"
-        cell.tweetContentLbl.text = "Tweet"
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary //extract key from parent dic
+        
+        cell.userNameLbl.text = user["name"] as? String
+        cell.tweetContentLbl.text = tweetArray[indexPath.row]["text"] as? String
+        
+        //convert url to image
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        if let imageData = data{
+            cell.profileImg.image = UIImage(data: imageData)
+        }
         
         return cell
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return tweetArray.count
     }
 }
